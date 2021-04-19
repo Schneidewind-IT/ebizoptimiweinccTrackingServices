c47cd76bb74a6c5186a47bdf9f0c343feb2cd434
Reinhard Schneidewind <reinhard@schneidewind.it>
2021-04-19 14:09:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# WeinCC Tracking Services
+
+Track clickthroughs and conversions and get now insights to improve your product listing on wein.cc. Get reports that let you know how to adjust your bids.
+
+## Installation
+
+- Copy all files from `src/` folder to your Magento root directory.
+- Clear Magento Cache
+- Relogin into the Magento Backend
+
+## Documentation
+
+https://www.wein.cc/en-de/e_technik_tracking_services.php
---
+++ src/app/code/community/SchneidewindIt/EbizoptimiweinccTrackingServices/Helper/Data.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * @category   SchneidewindIt
+ * @package    SchneidewindIt_ebizoptimiweinccTrackingServices
+ * @copyright  Copyright (c) 2021 BlueVisionTec UG (haftungsbeschränkt) (http://www.bluevisiontec.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Data Helper
+ *
+ * @category   SchneidewindIt
+ * @package    SchneidewindIt_ebizoptimiweinccTrackingServices
+ * @author     Reinhard Schneidewind <reinhard@schneidewind.it>
+ */
+class SchneidewindIt_EbizoptimiweinccTrackingServices_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
